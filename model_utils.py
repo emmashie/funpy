e744bbdcc42f4ff29063af26583b15cca1a619f2
emmashie <emmashie@users.noreply.github.com>
2023-07-26 17:04:31 -0700

    Update model_utils.py
    fixed memory issue

--- model_utils.py
+++ model_utils.py
@@ -13,7 +13,7 @@ def load_masked_variable(fdir, var, varfile, mask, maskfile):
     var_unmasked = xr.open_mfdataset(os.path.join(fdir, varfile))[var]
     x = np.asarray(xr.open_mfdataset(os.path.join(fdir, varfile))['x'])
     y = np.asarray(xr.open_mfdataset(os.path.join(fdir, varfile))['y'])
-    mask = np.asarray(mask_ds[mask])
+    mask = mask_ds[mask]
     var_masked = ma.masked_where(mask==0, var_unmasked)
     return var_masked, x, y 
 
